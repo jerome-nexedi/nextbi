02e33356843bd0fdccbaa1fafbf721f6a5cd825e
undyliu@126.com <undyliu@126.com@95771b4f-3275-74dd-227b-ec539d8822a0>
2012-07-16 11:27:23 +0000

    支持servlet3.0，java代码样式格式化

--- trunk/plugins/com.seekon.nextbi.spreadsheet/src/com/seekon/nextbi/spreadsheet/servlet/JsonResponseServlet.java
+++ trunk/plugins/com.seekon.nextbi.spreadsheet/src/com/seekon/nextbi/spreadsheet/servlet/JsonResponseServlet.java
@@ -6,23 +6,24 @@ import java.io.PrintWriter;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletResponse;
 
-public class JsonResponseServlet extends HttpServlet{
+public class JsonResponseServlet extends HttpServlet {
 
-	private static final long serialVersionUID = 7545512402639631614L;
-	
-	protected void flushJsonResult(HttpServletResponse response, String result) throws IOException{
-		if(result == null){
-			return;
-		}
-		
-		String contentType = "application/json; charset=UTF-8";
-		response.setContentType(contentType);
-		response.setHeader("Content-Length", result.length() + "");
+  private static final long serialVersionUID = 7545512402639631614L;
+
+  protected void flushJsonResult(HttpServletResponse response, String result)
+    throws IOException {
+    if (result == null) {
+      return;
+    }
+
+    String contentType = "application/json; charset=UTF-8";
+    response.setContentType(contentType);
+    response.setHeader("Content-Length", result.length() + "");
     response.setHeader("Cache-Control", "no-cache, must-revalidate");
     response.setHeader("Content-Type", contentType);
-    
+
     PrintWriter out = response.getWriter();
     out.println(result);
     out.flush();
-	}
+  }
 }
