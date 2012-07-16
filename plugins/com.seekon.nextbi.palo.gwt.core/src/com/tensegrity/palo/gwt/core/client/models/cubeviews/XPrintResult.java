1920e49ed2f9872559f01205f9675f40a7d3922e
undyliu <undyliu@126.com>
2012-07-16 11:28:52 +0000

    支持servlet3.0，java代码样式格式化

--- plugins/com.seekon.nextbi.palo.gwt.core/src/com/tensegrity/palo/gwt/core/client/models/cubeviews/XPrintResult.java
+++ plugins/com.seekon.nextbi.palo.gwt.core/src/com/tensegrity/palo/gwt/core/client/models/cubeviews/XPrintResult.java
@@ -3,22 +3,23 @@ package com.tensegrity.palo.gwt.core.client.models.cubeviews;
 import com.google.gwt.user.client.rpc.IsSerializable;
 
 public class XPrintResult implements IsSerializable {
-	private String fileName;
-	private XViewModel view;
-	
-	public XPrintResult() {		
-	}
-	
-	public XPrintResult(String fName, XViewModel view) {
-		this.fileName = fName;
-		this.view = view;
-	}
-	
-	public XViewModel getView() {
-		return view;
-	}
-	
-	public String getFilename() {
-		return fileName;
-	}
+  private String fileName;
+
+  private XViewModel view;
+
+  public XPrintResult() {
+  }
+
+  public XPrintResult(String fName, XViewModel view) {
+    this.fileName = fName;
+    this.view = view;
+  }
+
+  public XViewModel getView() {
+    return view;
+  }
+
+  public String getFilename() {
+    return fileName;
+  }
 }
