52a1e23175e86fe62336f24059d9264af9236a08
undyliu <undyliu@126.com>
2012-07-16 11:27:02 +0000

    支持servlet3.0，java代码样式格式化

--- plugins/com.seekon.bicp.springsupport/src/com/seekon/bicp/springsupport/orm/jpa/support/OsgiSupportOpenEntityManagerInViewFilter.java
+++ plugins/com.seekon.bicp.springsupport/src/com/seekon/bicp/springsupport/orm/jpa/support/OsgiSupportOpenEntityManagerInViewFilter.java
@@ -8,40 +8,47 @@ import org.springframework.util.StringUtils;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.context.support.WebApplicationContextUtils;
 
-public class OsgiSupportOpenEntityManagerInViewFilter extends OpenEntityManagerInViewFilter{
+public class OsgiSupportOpenEntityManagerInViewFilter extends
+  OpenEntityManagerInViewFilter {
 
-	private String contextAttribute;
+  private String contextAttribute;
 
-	public String getContextAttribute() {
-		return contextAttribute;
-	}
+  public String getContextAttribute() {
+    return contextAttribute;
+  }
 
-	public void setContextAttribute(String contextAttribute) {
-		this.contextAttribute = contextAttribute;
-	}
-	
-	@Override
-	protected EntityManagerFactory lookupEntityManagerFactory() {	
-		WebApplicationContext wac = null;
-		String attrName = getContextAttribute();
-		if (attrName != null) {
-			wac = WebApplicationContextUtils.getWebApplicationContext(getServletContext(), attrName);
-		}else{
-			wac = WebApplicationContextUtils.getRequiredWebApplicationContext(getServletContext());
-		}
-		if (wac == null) {
-			throw new IllegalStateException("No WebApplicationContext found: no ContextLoaderListener registered?");
-		}
-		
+  public void setContextAttribute(String contextAttribute) {
+    this.contextAttribute = contextAttribute;
+  }
+
+  @Override
+  protected EntityManagerFactory lookupEntityManagerFactory() {
+    WebApplicationContext wac = null;
+    String attrName = getContextAttribute();
+    if (attrName != null) {
+      wac = WebApplicationContextUtils.getWebApplicationContext(getServletContext(),
+        attrName);
+    } else {
+      wac = WebApplicationContextUtils
+        .getRequiredWebApplicationContext(getServletContext());
+    }
+    if (wac == null) {
+      throw new IllegalStateException(
+        "No WebApplicationContext found: no ContextLoaderListener registered?");
+    }
+
     String emfBeanName = getEntityManagerFactoryBeanName();
     String puName = getPersistenceUnitName();
     if (StringUtils.hasLength(emfBeanName)) {
-      return (EntityManagerFactory)wac.getBean(emfBeanName, EntityManagerFactory.class);
+      return (EntityManagerFactory) wac.getBean(emfBeanName,
+        EntityManagerFactory.class);
     }
-    if ((!StringUtils.hasLength(puName)) && (wac.containsBean("entityManagerFactory"))) {
-      return (EntityManagerFactory)wac.getBean("entityManagerFactory", EntityManagerFactory.class);
+    if ((!StringUtils.hasLength(puName))
+      && (wac.containsBean("entityManagerFactory"))) {
+      return (EntityManagerFactory) wac.getBean("entityManagerFactory",
+        EntityManagerFactory.class);
     }
 
     return EntityManagerFactoryUtils.findEntityManagerFactory(wac, puName);
-	}
+  }
 }
--- plugins/com.seekon.bicp.springsupport/src/com/seekon/bicp/springsupport/web/servlet/OsgiSupportDispatcherServlet.java
+++ plugins/com.seekon.bicp.springsupport/src/com/seekon/bicp/springsupport/web/servlet/OsgiSupportDispatcherServlet.java
@@ -7,33 +7,35 @@ import org.springframework.osgi.web.context.support.OsgiBundleXmlWebApplicationC
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
-public class OsgiSupportDispatcherServlet extends DispatcherServlet{
+public class OsgiSupportDispatcherServlet extends DispatcherServlet {
 
-	private static final long serialVersionUID = 5001619869890003091L;
+  private static final long serialVersionUID = 5001619869890003091L;
 
-	@Override
-	protected WebApplicationContext createWebApplicationContext(
-			ApplicationContext parent) {
-		ServletContext servletContext = getServletContext();
-		if(parent == null && servletContext != null){
-			String bundleName = servletContext.getInitParameter("bundle.name");
-			parent = (ApplicationContext)servletContext.getAttribute("context_" + bundleName);
-		}
-		
-		if(!(parent instanceof OsgiBundleXmlWebApplicationContext)){
-			throw new RuntimeException("parent must be the instance of OsgiBundleXmlWebApplicationContext.");
-		}
-		
-		OsgiBundleXmlWebApplicationContext _parent = (OsgiBundleXmlWebApplicationContext)parent;
-		OsgiBundleXmlWebApplicationContext wac = new OsgiBundleXmlWebApplicationContext();
-		wac.setParent(_parent);
-		wac.setConfigLocation(getContextConfigLocation());
-		wac.setBundleContext(_parent.getBundleContext());
-		wac.setServletContext(_parent.getServletContext());
-		
-		configureAndRefreshWebApplicationContext(wac);
+  @Override
+  protected WebApplicationContext createWebApplicationContext(
+    ApplicationContext parent) {
+    ServletContext servletContext = getServletContext();
+    if (parent == null && servletContext != null) {
+      String bundleName = servletContext.getInitParameter("bundle.name");
+      parent = (ApplicationContext) servletContext.getAttribute("context_"
+        + bundleName);
+    }
 
-		return wac;
-	}
-	
+    if (!(parent instanceof OsgiBundleXmlWebApplicationContext)) {
+      throw new RuntimeException(
+        "parent must be the instance of OsgiBundleXmlWebApplicationContext.");
+    }
+
+    OsgiBundleXmlWebApplicationContext _parent = (OsgiBundleXmlWebApplicationContext) parent;
+    OsgiBundleXmlWebApplicationContext wac = new OsgiBundleXmlWebApplicationContext();
+    wac.setParent(_parent);
+    wac.setConfigLocation(getContextConfigLocation());
+    wac.setBundleContext(_parent.getBundleContext());
+    wac.setServletContext(_parent.getServletContext());
+
+    configureAndRefreshWebApplicationContext(wac);
+
+    return wac;
+  }
+
 }
