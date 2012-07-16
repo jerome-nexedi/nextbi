28fec7f55fdf3dd878b7cbe85b3de71bfc1e0b51
undyliu <undyliu@126.com>
2012-07-16 11:27:23 +0000

    支持servlet3.0，java代码样式格式化

--- plugins/com.seekon.nextbi.spreadsheet/src/com/seekon/nextbi/spreadsheet/servlet/UbServlet.java
+++ plugins/com.seekon.nextbi.spreadsheet/src/com/seekon/nextbi/spreadsheet/servlet/UbServlet.java
@@ -11,66 +11,65 @@ import javax.servlet.http.HttpServletResponse;
 import org.json.JSONArray;
 import org.json.JSONException;
 
-public class UbServlet extends JsonResponseServlet{
+public class UbServlet extends JsonResponseServlet {
 
-	private static final long serialVersionUID = 8427700133133008903L;
-	
-	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-			throws ServletException, IOException {
-		this.doPost(req, resp);
-	}
-	
-	@Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
-			throws ServletException, IOException {
-//		Enumeration enum0 = req.getParameterNames();
-//		if(enum0.hasMoreElements()){
-//			String name = enum0.nextElement().toString();
-//			System.out.println(name + ":" + req.getParameter(name));
-//		}
-		
-		String body = "";
-		ServletInputStream sis = req.getInputStream();
-		if(sis != null){
-			byte[] temp = new byte[1024];
-			int length = 0;
-			while((length = sis.read(temp)) > 0){
-				if(length < temp.length){
-					body += new String(temp, 0, length);
-				}else{
-					body += new String(temp);
-				}
-			}
-		}
-		
-		if(body != null && body.trim().length() > 0){
-//			try {
-//				JSONArray jsonBody = new JSONArray(body);
-//				
-//			} catch (JSONException e) {
-//				e.printStackTrace();
-//			}
-			
-			String result = null;
-			if(body.startsWith("[[\"olst")){
-				result = "[[true,{\"b74c9c04-9714-11e1-8ff-35e69c1cf085\":\"Sheet1\"}],[true,{}],[true,\"b74c9c04-9714-11e1-8ff-35e69c1cf085\"],[true,[]]]";
-			}else if(body.startsWith("[[\"gdcr")){
-				result = "[[true,[64,20]],[true,[]],[true,[]],[true,[1,1]],[true,[]]]";
-			}else if(body.startsWith("[[\"wget")){
-				result = "[[true,[]],[true,[]],[true,[]],[true,[]]]";
-			}else if(body.startsWith("[[\"grar")){
-				result = "[[true]]";
-			}else if(body.startsWith("[[\"lock")){
-				result = "[[true,\"\"]]";
-			}else if(body.startsWith("[[\"ctrn")){
-				result = "[[true,[1]]]";
-			}
-			System.out.println("####body:" + body);
-			
-			flushJsonResult(resp, result);
-		}
-	}
-	
-	
+  private static final long serialVersionUID = 8427700133133008903L;
+
+  @Override
+  protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+    throws ServletException, IOException {
+    this.doPost(req, resp);
+  }
+
+  @Override
+  protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+    throws ServletException, IOException {
+    // Enumeration enum0 = req.getParameterNames();
+    // if(enum0.hasMoreElements()){
+    // String name = enum0.nextElement().toString();
+    // System.out.println(name + ":" + req.getParameter(name));
+    // }
+
+    String body = "";
+    ServletInputStream sis = req.getInputStream();
+    if (sis != null) {
+      byte[] temp = new byte[1024];
+      int length = 0;
+      while ((length = sis.read(temp)) > 0) {
+        if (length < temp.length) {
+          body += new String(temp, 0, length);
+        } else {
+          body += new String(temp);
+        }
+      }
+    }
+
+    if (body != null && body.trim().length() > 0) {
+      // try {
+      // JSONArray jsonBody = new JSONArray(body);
+      //				
+      // } catch (JSONException e) {
+      // e.printStackTrace();
+      // }
+
+      String result = null;
+      if (body.startsWith("[[\"olst")) {
+        result = "[[true,{\"b74c9c04-9714-11e1-8ff-35e69c1cf085\":\"Sheet1\"}],[true,{}],[true,\"b74c9c04-9714-11e1-8ff-35e69c1cf085\"],[true,[]]]";
+      } else if (body.startsWith("[[\"gdcr")) {
+        result = "[[true,[64,20]],[true,[]],[true,[]],[true,[1,1]],[true,[]]]";
+      } else if (body.startsWith("[[\"wget")) {
+        result = "[[true,[]],[true,[]],[true,[]],[true,[]]]";
+      } else if (body.startsWith("[[\"grar")) {
+        result = "[[true]]";
+      } else if (body.startsWith("[[\"lock")) {
+        result = "[[true,\"\"]]";
+      } else if (body.startsWith("[[\"ctrn")) {
+        result = "[[true,[1]]]";
+      }
+      System.out.println("####body:" + body);
+
+      flushJsonResult(resp, result);
+    }
+  }
+
 }
