51f0def1faf944d38e8658829116d7e835db57d3
undyliu@126.com <undyliu@126.com@95771b4f-3275-74dd-227b-ec539d8822a0>
2012-07-16 00:52:40 +0000

    增加mondrian-olap引擎插件

---
+++ trunk/plugins/com.seekon.nextbi.mondrian/src/main/mondrian/olap/QueryCanceledException.java
@@ -0,0 +1,36 @@
+/*
+// $Id: //open/mondrian/src/main/mondrian/olap/QueryCanceledException.java#3 $
+// This software is subject to the terms of the Eclipse Public License v1.0
+// Agreement, available at the following URL:
+// http://www.eclipse.org/legal/epl-v10.html.
+// Copyright (C) 2004-2005 TONBELLER AG
+// Copyright (C) 2005-2007 Julian Hyde and others
+// All Rights Reserved.
+// You must accept the terms of that agreement to use this software.
+ */
+package mondrian.olap;
+
+/**
+ * Exception which indicates that a query was canceled by an end-user.
+ * 
+ * <p>
+ * See also {@link mondrian.olap.QueryTimeoutException}, which indicates that a
+ * query was canceled automatically due to a timeout.
+ * 
+ * @version $Id:
+ *          //open/mondrian/src/main/mondrian/olap/QueryCanceledException.java#3
+ *          $
+ */
+public class QueryCanceledException extends ResultLimitExceededException {
+	/**
+	 * Creates a QueryCanceledException.
+	 * 
+	 * @param message
+	 *          Localized error message
+	 */
+	public QueryCanceledException(String message) {
+		super(message);
+	}
+}
+
+// End QueryCanceledException.java
---
+++ trunk/plugins/com.seekon.nextbi.mondrian/src/main/mondrian/spi/PropertyFormatter.java
@@ -0,0 +1,25 @@
+/*
+// $Id: //open/mondrian/src/main/mondrian/spi/PropertyFormatter.java#1 $
+// This software is subject to the terms of the Eclipse Public License v1.0
+// Agreement, available at the following URL:
+// http://www.eclipse.org/legal/epl-v10.html.
+// Copyright (C) 2004-2005 TONBELLER AG
+// Copyright (C) 2006-2011 Julian Hyde and others
+// All Rights Reserved.
+// You must accept the terms of that agreement to use this software.
+ */
+package mondrian.spi;
+
+import mondrian.olap.Member;
+
+/**
+ * SPI to redefine a member property display string.
+ * 
+ * @version $Id: //open/mondrian/src/main/mondrian/spi/PropertyFormatter.java#1
+ *          $
+ */
+public interface PropertyFormatter {
+	String formatProperty(Member member, String propertyName, Object propertyValue);
+}
+
+// End PropertyFormatter.java
