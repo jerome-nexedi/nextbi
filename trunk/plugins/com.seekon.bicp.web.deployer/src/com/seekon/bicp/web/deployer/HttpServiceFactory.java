b5e6eef13f54fa44eca381ab8756d9575b96cd8c
undyliu@126.com <undyliu@126.com@95771b4f-3275-74dd-227b-ec539d8822a0>
2012-07-16 00:11:23 +0000

    web插件加载部署的支撑

---
+++ trunk/plugins/com.seekon.bicp.web.deployer/src/com/seekon/bicp/web/deployer/HttpServiceFactory.java
@@ -0,0 +1,20 @@
+package com.seekon.bicp.web.deployer;
+
+import org.apache.felix.http.api.ExtHttpService;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceReference;
+
+public final class HttpServiceFactory {
+
+	public static ExtHttpService getHttpService() {
+		BundleContext bundleContext = Activator.instance.bundleContext;
+		System.out.println("bundleContext :" + bundleContext);
+		ServiceReference reference = bundleContext
+				.getServiceReference("org.osgi.service.http.HttpService");
+		System.out.println("reference :" + reference);
+		ExtHttpService httpService = (ExtHttpService) bundleContext
+				.getService(reference);
+		System.out.println("httpService :" + httpService);
+		return httpService;
+	}
+}
---
+++ trunk/plugins/com.seekon.bicp.web.deployer/src/com/seekon/bicp/web/deployer/ModuleLoader.java
@@ -0,0 +1,27 @@
+package com.seekon.bicp.web.deployer;
+
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+
+public interface ModuleLoader {
+
+//  /**
+//   * 初始化，可以做一些解析工作，如读取配置
+//   * @param bundleContext
+//   */
+//  public void init(BundleContext bundleContext);
+
+  /**
+   * 执行加载，
+   * @param bundle
+   * @throws LoadException
+   */
+  public void load(Bundle bundle) throws LoadException;
+
+  /**
+   * 卸载已加载的模块
+   * @param bundle
+   * @throws LoadException
+   */
+  public void unload(Bundle bundle) throws LoadException;
+}
