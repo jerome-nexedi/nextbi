af06fa7220402d6e52dd8013e4e9524ad5607538
undyliu <undyliu@126.com>
2012-07-16 00:15:43 +0000

    增加sso单点登录的cas插件

---
+++ plugins/com.seekon.bicp.cas/src/org/jasig/cas/util/MD5PasswordEncryptor.java
@@ -0,0 +1,25 @@
+package org.jasig.cas.util;
+
+import java.security.MessageDigest;
+
+import sun.misc.BASE64Encoder;
+
+public class MD5PasswordEncryptor {
+
+	public static final String encrypt(String pass) {
+		if ((pass == null) || (pass.equals(""))) {
+			return "";
+		}
+		MessageDigest md = null;
+		try {
+			md = MessageDigest.getInstance("MD5");
+			md.update(pass.getBytes("UTF-8"));
+			byte[] raw = md.digest();
+			String hash = new BASE64Encoder().encode(raw);
+			return hash;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return pass;
+	}
+}
---
+++ plugins/com.seekon.bicp.cas/src/org/jasig/portal/cas/authentication/handler/support/UserPasswordDao.java
@@ -0,0 +1,6 @@
+package org.jasig.portal.cas.authentication.handler.support;
+
+public abstract interface UserPasswordDao
+{
+  public abstract String getPasswordHash(String paramString);
+}
