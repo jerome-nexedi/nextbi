74f80abbe66fd39a87f39ff6571033e8edf1dd7f
undyliu <undyliu@126.com>
2012-07-19 13:49:01 +0000

    重构：采用equinox的http服务注册方式

--- plugins/com.seekon.bicp.web.deployer/src/com/seekon/bicp/web/deployer/HttpServiceFactory.java
+++ plugins/com.seekon.bicp.web.deployer/src/com/seekon/bicp/web/deployer/HttpServiceFactory.java
@@ -1,18 +1,18 @@
 package com.seekon.bicp.web.deployer;
 
-import org.apache.felix.http.api.ExtHttpService;
+import org.eclipse.equinox.http.servlet.ExtendedHttpService;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceReference;
 
 public final class HttpServiceFactory {
 
-  public static ExtHttpService getHttpService() {
+  public static ExtendedHttpService getHttpService() {
     BundleContext bundleContext = Activator.instance.bundleContext;
     System.out.println("bundleContext :" + bundleContext);
     ServiceReference reference = bundleContext
       .getServiceReference("org.osgi.service.http.HttpService");
     System.out.println("reference :" + reference);
-    ExtHttpService httpService = (ExtHttpService) bundleContext
+    ExtendedHttpService httpService = (ExtendedHttpService) bundleContext
       .getService(reference);
     System.out.println("httpService :" + httpService);
     return httpService;
