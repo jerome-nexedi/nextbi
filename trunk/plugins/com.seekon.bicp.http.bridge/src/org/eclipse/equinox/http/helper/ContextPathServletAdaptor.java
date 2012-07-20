82f9b7edc0b3107e74915434d19e620bda8aaacf
undyliu@126.com <undyliu@126.com@95771b4f-3275-74dd-227b-ec539d8822a0>
2012-07-20 00:11:20 +0000

    去掉反射机制获取contextPath

--- trunk/plugins/com.seekon.bicp.http.bridge/src/org/eclipse/equinox/http/helper/ContextPathServletAdaptor.java
+++ trunk/plugins/com.seekon.bicp.http.bridge/src/org/eclipse/equinox/http/helper/ContextPathServletAdaptor.java
@@ -9,12 +9,25 @@ package org.eclipse.equinox.http.helper;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.lang.reflect.Method;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.*;
+import java.util.Enumeration;
+import java.util.EventListener;
+import java.util.Map;
+import java.util.Set;
 
-import javax.servlet.*;
+import javax.servlet.Filter;
+import javax.servlet.FilterRegistration;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.Servlet;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.SessionCookieConfig;
+import javax.servlet.SessionTrackingMode;
 import javax.servlet.FilterRegistration.Dynamic;
 import javax.servlet.descriptor.JspConfigDescriptor;
 import javax.servlet.http.HttpServletRequest;
@@ -298,13 +311,7 @@ public class ContextPathServletAdaptor implements Servlet {
 
     // Added in Servlet 2.5
     public String getContextPath() {
-      try {
-        Method getContextPathMethod = delegate.getClass().getMethod("getContextPath", null); //$NON-NLS-1$
-        return (String) getContextPathMethod.invoke(delegate, null);
-      } catch (Exception e) {
-        // ignore
-      }
-      return null;
+      return delegate.getContextPath();
     }
   }
 
